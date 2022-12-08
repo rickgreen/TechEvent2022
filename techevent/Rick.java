bc12a693101bf6f7621a259563a0ebff05373914
Rick Green <rick@rickgreen.nl>
2022-12-08 18:11:35 +0100

    initial

---
+++ techevent/Rick.java
@@ -0,0 +1,61 @@
+package techevent;
+
+
+import robocode.HitByBulletEvent;
+import robocode.HitRobotEvent;
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+import static robocode.util.Utils.normalRelativeAngleDegrees;
+
+// import java.awt.*;
+
+
+public class Rick extends Robot {
+	int dist = 50; // distance to move when we're hit
+
+
+	public void run() {
+
+		// Spin the gun around slowly... forever
+		while (true) {
+			turnGunRight(5);
+		}
+	}
+
+	/**
+	 * onScannedRobot:  Fire!
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// If the other robot is close by, and we have plenty of life,
+		// fire hard!
+		if (e.getDistance() < 50 && getEnergy() > 50) {
+			fire(3);
+		} // otherwise, fire 1.
+		else {
+			fire(1);
+		}
+		// Call scan again, before we turn the gun
+		scan();
+	}
+
+	/**
+	 * onHitByBullet:  Turn perpendicular to the bullet, and move a bit.
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		turnRight(normalRelativeAngleDegrees(90 - (getHeading() - e.getHeading())));
+
+		ahead(dist);
+		dist *= -1;
+		scan();
+	}
+
+	/**
+	 * onHitRobot:  Aim at it.  Fire Hard!
+	 */
+	public void onHitRobot(HitRobotEvent e) {
+		double turnGunAmt = normalRelativeAngleDegrees(e.getBearing() + getHeading() - getGunHeading());
+
+		turnGunRight(turnGunAmt);
+		fire(3);
+	}
+}
